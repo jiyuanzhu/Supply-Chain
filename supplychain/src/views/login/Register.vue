21c2584aa9f21ccac8563a23d3053858deebf0ba
jiyuanzhu <44122470+jiyuanzhu@users.noreply.github.com>
2019-07-04 12:06:01 +0800

    Update Register.vue

--- supplychain/src/views/login/Register.vue
+++ supplychain/src/views/login/Register.vue
@@ -3,25 +3,24 @@
         <div id="regtop">
             <TopNav></TopNav>
         </div>
-        <div id="regbody">
-            <div id="register">
-                <div id="logintitle">
-                    <p>登录</p>
-                </div>
-                <div id="username">
-                    <p>用户名</p>
-                    <el-input type="text"></el-input>
-                </div>
-                <div id="password">
-                    <p>密码</p>
-                    <el-input type="text"></el-input>
-                </div>
-                <div class="doubut">
-                    <el-button @click="bankl" class="bankl">登录</el-button>
-                    <el-button @click="registerbt" class="registerbt">注册</el-button>
-                </div>
+        <div id="register">
+            <div id="regtitle">
+                <p>注册</p>
             </div>
-      </div>
+            <div id="inputusername">
+                <p>请输入用户名</p>
+                <el-input type="text"></el-input>
+            </div>
+            <div id="inputpassword">
+                <p>请输入密码</p>
+                <el-input type="text"></el-input>
+            </div>
+            <div id="inputpassword1">
+                <p>请再次输入密码</p>
+                <el-input type="text"></el-input>
+            </div>
+            <el-button @click="registerbt" class="registerbt">注册</el-button>
+        </div>
     </div>
 </template>
 
@@ -40,6 +39,11 @@ export default {
         backgroundSize: "100%",
       },
     }
+  },
+  methods:{
+      registerbt(){
+      this.$router.replace('/')
+    }
   }
 }
 </script>
@@ -48,14 +52,63 @@ export default {
 .regback{
   height:750px;
   width:100%;
+  text-align: center;
 }
 
+
 #register{
-  height:100%;
-  width:100%;
+  height:70%;
+  width:50%;
   background-color: rgb(252, 247, 252);
   border-radius:30px;
   box-shadow: 1px #383737;
+  text-align: center;
+  margin:auto;
+}
+
+#regtitle{
+  height:20%;
+  width:100%;
+  text-align: center;
+  font-family:"华文行楷"; 
+  font-size:30px;
+  padding-top:2px;
+}
+
+#inputusername{
+  margin:auto;
+  height:16%;
+  width:62%;
+  text-align: left;
+  font-family:"华文行楷"; 
+  font-size:20px;
+}
+
+#inputpassword{
+  margin:auto;
+  height:16%;
+  width:62%;
+  text-align: left;
+  font-family:"华文行楷"; 
+  font-size:20px;
+}
+
+#inputpassword1{
+  margin:auto;
+  height:16%;
+  width:62%;
+  text-align: left;
+  font-family:"华文行楷"; 
+  font-size:20px;
+}
+
+.registerbt{
+  background-color: rgb(17, 212, 226);
+  width:100px;
+  height:45px;
+  margin-top: 25px;
+  font-family:"华文行楷"; 
+  font-size:25px;
   text-align: center;
 }
 </style>
